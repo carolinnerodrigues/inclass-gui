63378814d984f7c5df9dcaa7718d2e9074cf14e9
Carolinne Rodrigues <carolinne.rodrigues@optimuminterfaces.com.br>
2019-09-29 13:01:48 -0300

    Adiciona novo layout navbar

--- src/components/molecules/Navbar/Navbar.js
+++ src/components/molecules/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 // import { Link } from 'react-router-dom'
 import './Navbar.scss'
+import {Button} from "../../atoms/Button/Button"
 
 import AuthService from '../../../services/auth'
 
@@ -12,10 +13,16 @@ export class NavbarComponent extends Component {
 
   render() {
     return (
-      <div id="Navabar">
-        <nav class="navbar navbar-dark"> 
-          <h2>InClass</h2>
-        </nav> 
+      <div id="Navbar">
+        <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+          <h5 class="my-0 mr-md-auto font-weight-normal">InClass</h5>
+          <nav class="my-2 my-md-0 mr-md-3">
+            <a class="p-2 text-dark" href="#">Modelar</a>
+            <a class="p-2 text-dark" href="#">Cadastrar Turmas</a>
+            <a class="p-2 text-dark" href="#">Cadastrar Salas</a>
+          </nav>
+          <Button>Logout</Button>
+        </div>
       </div>
     )
   }
